d8cf44cbb605ee1661c700d217b7c99d1c05a360
magnusgp <s204075@student.dtu.dk>
2023-01-05 15:17:04 +0100

    new initial commit
    added all files from the cookie cutter structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A repository for course work in DTU Course 02476 - Machine Learning Operations',
+    author='Magnus Guldberg Pedersen',
+    license='MIT',
+)
