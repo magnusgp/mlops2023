8c1d579ea3a16aee762da5e835374a00038c3657
dinbaba <s204075@student.dtu.dk>
2023-01-05 15:28:24 +0100

    added subdirectories

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -12,6 +12,7 @@ def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
+    
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
