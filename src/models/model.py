0836c3c287fe9a4b8b1bd5bccf55705171a0c11f
dinbaba <s204075@student.dtu.dk>
2023-01-09 12:28:05 +0100

    added pre-commit

--- src/models/model.py
+++ src/models/model.py
@@ -4,20 +4,15 @@ import torch.nn.functional as F
 class Model(nn.Module):
     def __init__(self):
         super().__init__()
+        # define the layers
         self.fc1 = nn.Linear(784, 256)
         self.fc2 = nn.Linear(256, 128)
         self.fc3 = nn.Linear(128, 64)
         self.fc4 = nn.Linear(64, 10)
-        
+        # add dropout layer
         self.dropout = nn.Dropout(p=0.2)
         
     def forward(self, x):
-        # if x.ndim != 1:
-        #   raise ValueError('Expected input to a 1D tensor')
-
-        # if x.shape[1] != 784:
-        #     raise ValueError('Expected each sample to have shape [784]')
-        
         # make sure input tensor is flattened
         x = x.view(x.shape[0], -1)
         
